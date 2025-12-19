f619aacb44b7f0e3b1b0cf8c48e55046efeade1a
Anurag-soni-8479 <anuragsoni7354@gmail.com>
2025-12-19 18:43:16 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> unnati khare</h1>
+      <h1> unnati khare ready to deploy</h1>
       
 
       
