d096bcbe8d9bf779d77587cf0639bd70809f9dc5
Anurag-soni-8479 <anuragsoni7354@gmail.com>
2025-12-19 18:04:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> vite + react</h1>
+      <h1> unnati khare</h1>
       
 
       
